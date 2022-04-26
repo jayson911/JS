4c9665a3325af66141a3a74539317fda30980950
jayson911 <jayson406@gmail.com>
2022-04-26 11:39:44 +0300

    hw4

--- hw4/main.js
+++ hw4/main.js
@@ -48,17 +48,55 @@
 // sayHello("man ");
 // sayHello("okten");
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 // function someTxtUlLi(text){
 //     for (let i = 0; i < 3; i++) {
+//         document.write(`<ul>Some Ul`);
 //         document.write(`<li>${text}</li>`);
-//
+//         document.write(`</ul>`);
 //     }
 // }
-// document.write(`<ul>Some Ul`);
 // someTxtUlLi('Hello my friend');
-// document.write(`</ul>`);
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+// function someArr(){
+//     for (let i = 0; i < arguments.length; i++) {
+//         let argument = arguments[i];
+//         console.log(argument);
+//     }
+// }
+// let elements = [123, 'hello', true];
+// someArr(elements);
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+// let users = [
+//     {id: 1, name: 'Vasya', age: 35},
+//     {id: 2, name: 'olya', age: 24},
+//     {id: 3, name: 'maksim', age: 45},
+//     {id: 4, name: 'andrey', age: 56}
+// ];
+// function iterator(users){
+//     for (let i = 0; i < users.length; i++) {
+//         let user = users[i];
+//         console.log(user);
+//         document.write(`<div>${user.id}, ${user.name}, ${user.age}</div>`);
+//     }
+// }
+// iterator(users);
 // - створити функцію яка повертає найменьше число з масиву
+// function minNum(a, b){
+//     if (a < b){
+//         return a;
+//     }else return b;
+// }
+// let num = minNum(
+//     prompt('num1'),
+//     prompt('num2')
+// );
+// console.log(num);
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+function sum(x){
+    let s = 0;
+    for (let i = 0; i < x.length; i++) {
+        s += x[i];
+    } return s;
+}
+console.log(sum([1, 5, 55]));
