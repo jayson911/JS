9ff5206f6ae75bcd94142293ec303bd2231d9c84
jayson911 <jayson406@gmail.com>
2022-05-17 00:56:05 +0300

    hw9

---
+++ hw9/main.js
@@ -0,0 +1,52 @@
+// Все робити за допомоги js.
+// - створити блок,
+//     - додати йому класи wrap, collapse, alpha, beta
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// - додати цей блок в body.
+// - клонувати його повністю, та додати клон в body.
+// const divka = document.createElement('div');
+// console.log(divka);
+// divka.classList.add('wrap', 'collapse', 'alpha', 'beta');
+// divka.innerText = 'hello octen';
+// divka.style.backgroundColor = 'aqua';
+// divka.style.color = 'red';
+// divka.style.fontSize = '40px';
+// document.body.appendChild(divka);
+// document.body.appendChild(divka.cloneNode(true));
+// - Є масив:
+//     ['Main','Products','About us','Contacts']
+// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
+// Завдання робити через цикли.
+
+let mass = ['Main','Products','About us','Contacts'];
+for (const mass1 of mass) {
+    const takeUl = document.getElementsByClassName('menu');
+    takeUl.innerHTML = `<li/> ${mass}`;
+    document.appendChild(takeUl);
+}
+
+
+
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// Завдання робити через цикли.
+//
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+//     Завдання робити через цикли.
